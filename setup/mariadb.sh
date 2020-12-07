fb121c2b4de70d08c296259257d5f653736da6a6
gluecifer <>
2020-12-08 00:23:30 +0100

    add mariadb.sh

---
+++ setup/mariadb.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+source setup/functions.sh
+
+apt_install mariadb-server
+
+# Create an auto-configuration file to fill in database settings
+#  when the install script is run. Make an administrator account
+# here or else the install can't finish.
+adminpassword=$(dd if=/dev/urandom bs=1 count=40 2>/dev/null | sha1sum | fold -w 30 | head -n 1)
+cat > /usr/local/lib/owncloud/config/autoconfig.php <<EOF;
+<?php
+\$AUTOCONFIG = array (
+  # storage/database
+  'directory' => '$STORAGE_ROOT/owncloud',
+  'dbtype' => 'mysql',
+  # create an administrator account with a random password so that
+  # the user does not have to enter anything on first load of Nextcloud
+  'adminlogin'    => 'root',
+  'adminpass'     => '$adminpassword',
+);
+?>
+EOF
+
+# Harden the installation of mariadb:
+#
+cat > /tmp/mariadb_secure_install.sql <<EOF;
+UPDATE mysql.user SET Password=PASSWORD($adminpassword) WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+EOF
+
+# Run the script to harden the mariadb installation:
+mysql -u root < "/tmp/mariadb_secure_install.sql"
+
+# Remove the script:
+rm -f /tmp/mariadb_secure_install.sql
+
+service mysql stop
+
+# Backup the original mariadb configuration file. 
+mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
+
+if [ ! -d $STORAGE_ROOT/nextcloud/mysql ]; then
+    mysqldatadir=$($STORAGE_ROOT/nextcloud/mysql)
+    mkdir $mysqldatadir
+
+cat > /etc/mysql/my.cnf <<EOF;
+[client]
+default-character-set = utf8mb4
+port = 3306
+socket = /var/run/mysqld/mysqld.sock
+[mysqld_safe]
+log_error=/var/log/mysql/mysql_error.log
+nice = 0
+socket = /var/run/mysqld/mysqld.sock
+[mysqld]
+basedir = /usr
+bind-address = 127.0.0.1
+binlog_format = ROW
+bulk_insert_buffer_size = 16M
+character-set-server = utf8mb4
+collation-server = utf8mb4_general_ci
+concurrent_insert = 2
+connect_timeout = 5
+datadir = $mysqldatadir
+default_storage_engine = InnoDB
+expire_logs_days = 2
+general_log_file = /var/log/mysql/mysql.log
+general_log = 0
+innodb_buffer_pool_size = 1024M
+innodb_buffer_pool_instances = 1
+innodb_flush_log_at_trx_commit = 2
+innodb_log_buffer_size = 32M
+innodb_max_dirty_pages_pct = 90
+innodb_file_per_table = 1
+innodb_open_files = 400
+innodb_io_capacity = 4000
+innodb_flush_method = O_DIRECT
+key_buffer_size = 128M
+lc_messages_dir = /usr/share/mysql
+lc_messages = en_US
+log_bin = /var/log/mysql/mariadb-bin
+log_bin_index = /var/log/mysql/mariadb-bin.index
+log_error = /var/log/mysql/mysql_error.log
+log_slow_verbosity = query_plan
+log_warnings = 2
+long_query_time = 1
+max_allowed_packet = 16M
+max_binlog_size = 100M
+max_connections = 200
+max_heap_table_size = 64M
+myisam_recover_options = BACKUP
+myisam_sort_buffer_size = 512M
+port = 3306
+pid-file = /var/run/mysqld/mysqld.pid
+query_cache_limit = 2M
+query_cache_size = 64M
+query_cache_type = 1
+query_cache_min_res_unit = 2k
+read_buffer_size = 2M
+read_rnd_buffer_size = 1M
+skip-external-locking
+skip-name-resolve
+slow_query_log_file = /var/log/mysql/mariadb-slow.log
+slow-query-log = 1
+socket = /var/run/mysqld/mysqld.sock
+sort_buffer_size = 4M
+table_open_cache = 400
+thread_cache_size = 128
+tmp_table_size = 64M
+tmpdir = /tmp
+transaction_isolation = READ-COMMITTED
+#unix_socket=OFF
+user = mysql
+wait_timeout = 600
+[mysqldump]
+max_allowed_packet = 16M
+quick
+quote-names
+[isamchk]
+key_buffer = 16M
+EOF
+
+restart_service mysql
